bd3ef464a9933e13c2b1a7a1875c31ef36585ef0
Takayuki Kamiyama <karuma.reason@gmail.com>
2025-07-27 13:04:54 +0900

    Update.

--- rplugin/python3/deoplete/sources/kindness.py
+++ rplugin/python3/deoplete/sources/kindness.py
@@ -78,27 +78,27 @@ class Source(Base):
 
         # TraceBack.
         except Exception:
-            # Check, kindness.py path.
-            git_k = File.basename(
-                File.expand_path(
-                    "~/~/.vim/plugged/kindness/rplugin/python3/deoplete/sources/kindness.py"
-                ), ".py") + "_log"
-
             # Load/Create LogFile.
-            except_folder: Optional[str] = '~/' + git_k
-            except_file: Optional[str] = '~/' + git_k + '/error.log'
+            kindness: Optional[str] = os.path.expanduser('~/kindness_log/')
+            db_w: Optional[str] = os.path.expanduser(
+                '~/kindness_log/debug.log')
 
             # Load the dictionary.
-            if os.path.isdir(os.path.expanduser(except_folder)):
-                with open(os.path.expanduser(except_file), 'a') as log_py:
+            if os.path.isdir(kindness):
+                with open(db_w, 'a') as log_py:
                     traceback.print_exc(file=log_py)
 
                     # throw except.
                     raise RuntimeError from None
 
-            # skl_str Folder not found.
+            # kindness_log Foler not found.
             else:
-                raise ValueError("None, Please Check the kindness Folder.")
+                raise ValueError("None, Please Check the kindness_log Folder.")
+
+        # Custom Exception.
+        except ValueError as ext:
+            print(ext)
+            raise RuntimeError from None
 
         # Once Exec.
         finally:
