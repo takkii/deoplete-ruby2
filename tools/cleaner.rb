6c5144d830d7c5f09038ba2b0ddd60e1206ca4b0
Takayuki Kamiyama <karuma.reason@gmail.com>
2025-09-08 06:44:03 +0900

    Rename.

--- tools/cleaner.rb
+++ tools/cleaner.rb
@@ -3,14 +3,10 @@
 require 'fileutils'
 require 'open3'
 require 'readline'
+require 'bmi'
 
 # Create runner.
 class CleanRunner
-  # default encoding utf-8, change encode here.
-  def self.encoding_style
-    Encoding.default_internal = 'UTF-8'
-    Encoding.default_external = 'UTF-8'
-  end
 
   def self.delete
     puts ''
@@ -23,19 +19,16 @@ class CleanRunner
       sel.grep(/\A#{Regexp.quote word}/)
     }
 
-    # Check, kindness.py path
-    filepath = '~/.vim/plugged/kindness/rplugin/python3/deoplete/sources/kindness.py'.to_s
-    git_k = File.basename(File.expand_path(filepath), '.py')
-    kind_exist = "#{git_k}_log"
+    ruby2_exist = "deoplete-ruby2_log"
     encoding_style
 
     while (line = Readline.readline(""))
       line.chomp!
 
       if line.match?(sel[0])
-        FileUtils.rm_rf(File.expand_path("~/#{kind_exist}"))
+        FileUtils.rm_rf(File.expand_path("~/#{ruby2_exist}"))
         puts ''
-        puts 'Deleted, the existing kindness_log folder.'
+        puts 'Deleted, the existing deoplete-ruby2_log folder.'
         puts ''
         break
       elsif line.match?(sel[1])
@@ -53,21 +46,18 @@ class CleanRunner
   end
 
   def self.run
-    # Check, kindness.py path
-    filepath = '~/.vim/plugged/kindness/rplugin/python3/deoplete/sources/kindness.py'.to_s
-    git_k = File.basename(File.expand_path(filepath), '.py')
-    kind_exist = "#{git_k}_log"
+    ruby2_exist = "deoplete-ruby2_log"
     encoding_style
 
-    if Dir.exist?(File.expand_path("~/#{kind_exist}"))
+    if Dir.exist?(File.expand_path("~/#{ruby2_exist}"))
       puts ''
-      puts 'Already have a kindness_log folder.'
+      puts 'Already have a deoplete-ruby2_log folder.'
       delete
     else
-      FileUtils.mkdir('kindness_log')
-      FileUtils.mv("#{File.dirname(__FILE__)}/kindness_log", File.expand_path('~/'))
+      FileUtils.mkdir('deoplete-ruby2_log')
+      FileUtils.mv("#{File.dirname(__FILE__)}/deoplete-ruby2_log", File.expand_path('~/'))
       puts ''
-      puts 'Created, kindness_log folder.'
+      puts 'Created, deoplete-ruby2_log folder.'
       puts ''
     end
   end
